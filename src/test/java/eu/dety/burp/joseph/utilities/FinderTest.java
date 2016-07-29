7988fd3c8b77ae278a76931a01263839f00c4b74
Dennis Detering <dennis.detering@rub.de>
2016-07-29 20:02:31 +0200

    Add JWE Finder tests

--- src/test/java/eu/dety/burp/joseph/utilities/FinderTest.java
+++ src/test/java/eu/dety/burp/joseph/utilities/FinderTest.java
@@ -39,7 +39,7 @@ public class FinderTest {
     }
 
     @Test
-    public void checkJwtPatternWithInvalidEncodedHeaderJwtWithoutEndingDotInputReturnsFalse() {
+    public void checkJwtPatternWithInvalidEncodedHeaderJwtInputReturnsFalse() {
         String token = "SW52YWxpZCBIZWFkZXI.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg";
 
         assertFalse(Finder.checkJwtPattern(token));
@@ -53,7 +53,7 @@ public class FinderTest {
     }
 
     @Test
-    public void checkJwtPatternWithTwoComponentJwtInputReturnsFalse() {
+    public void checkJwtPatternWithTwoComponentJwtWithoutEndingDotInputReturnsFalse() {
         String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9";
 
         assertFalse(Finder.checkJwtPattern(token));
@@ -70,4 +70,33 @@ public class FinderTest {
         String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
         assertFalse(Finder.checkJwtPattern(token));
     }
+
+    @Test
+    public void checkJwePatternWithValidRsaJweInputReturnsTrue() {
+        String token = "eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.MeAxi3xwgEt4aVTHVCWtijYCKAzu4AWbu7WdtqzgxhVbBbXrW_mqo3DGXtNFeRTEEweGDf5g5-2osbFUyYIJsRXj8tyE5HRMzbHBGrimKTTk7HTLv1KSwnKXVCGjy6ROxXjAjKVwGU5MDwP28XwhGAlS6fCYx_nIk-J7wf9YV1NDtDxk9F-s10J9BHJ27-l1M1HIVv7McWw94837UY4fM63BLDAJ70KbQvh_LF8RMdI7F2WXe-vTEcwFmicDi9Q6yvRzbYUhL1I19K4PHs9gTHtM-icRbJzPJvm0HKv0gclsXcsQtZgHNFTm3uLYbV5azYu5g5a5Iey-H1dvtpfbIQ.cwy_JXzA6_ZrdDETzYuPbw.o0qbcF7WeGansYSyRR3nZ82MRVoGnyVt4MdZZ00IgGw.vDZidX32xbA7uEUa7GMyiA";
+
+        assertTrue(Finder.checkJwePattern(token));
+    }
+
+    @Test
+    public void checkJwePatternWithInvalidEncodedHeaderJweInputReturnsFalse() {
+        String token = "SW52YWxpZCBIZWFkZXI.MeAxi3xwgEt4aVTHVCWtijYCKAzu4AWbu7WdtqzgxhVbBbXrW_mqo3DGXtNFeRTEEweGDf5g5-2osbFUyYIJsRXj8tyE5HRMzbHBGrimKTTk7HTLv1KSwnKXVCGjy6ROxXjAjKVwGU5MDwP28XwhGAlS6fCYx_nIk-J7wf9YV1NDtDxk9F-s10J9BHJ27-l1M1HIVv7McWw94837UY4fM63BLDAJ70KbQvh_LF8RMdI7F2WXe-vTEcwFmicDi9Q6yvRzbYUhL1I19K4PHs9gTHtM-icRbJzPJvm0HKv0gclsXcsQtZgHNFTm3uLYbV5azYu5g5a5Iey-H1dvtpfbIQ.cwy_JXzA6_ZrdDETzYuPbw.o0qbcF7WeGansYSyRR3nZ82MRVoGnyVt4MdZZ00IgGw.vDZidX32xbA7uEUa7GMyiA\";";
+
+        assertFalse(Finder.checkJwePattern(token));
+    }
+
+    @Test
+    public void checkJwePatternWithFourComponentJweInputReturnsFalse() {
+        String token = "eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.MeAxi3xwgEt4aVTHVCWtijYCKAzu4AWbu7WdtqzgxhVbBbXrW_mqo3DGXtNFeRTEEweGDf5g5-2osbFUyYIJsRXj8tyE5HRMzbHBGrimKTTk7HTLv1KSwnKXVCGjy6ROxXjAjKVwGU5MDwP28XwhGAlS6fCYx_nIk-J7wf9YV1NDtDxk9F-s10J9BHJ27-l1M1HIVv7McWw94837UY4fM63BLDAJ70KbQvh_LF8RMdI7F2WXe-vTEcwFmicDi9Q6yvRzbYUhL1I19K4PHs9gTHtM-icRbJzPJvm0HKv0gclsXcsQtZgHNFTm3uLYbV5azYu5g5a5Iey-H1dvtpfbIQ.cwy_JXzA6_ZrdDETzYuPbw.o0qbcF7WeGansYSyRR3nZ82MRVoGnyVt4MdZZ00IgGw";
+
+        assertFalse(Finder.checkJwtPattern(token));
+    }
+
+    @Test
+    public void checkJwePatternWithSixComponentJweInputReturnsFalse() {
+        String token = "eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.MeAxi3xwgEt4aVTHVCWtijYCKAzu4AWbu7WdtqzgxhVbBbXrW_mqo3DGXtNFeRTEEweGDf5g5-2osbFUyYIJsRXj8tyE5HRMzbHBGrimKTTk7HTLv1KSwnKXVCGjy6ROxXjAjKVwGU5MDwP28XwhGAlS6fCYx_nIk-J7wf9YV1NDtDxk9F-s10J9BHJ27-l1M1HIVv7McWw94837UY4fM63BLDAJ70KbQvh_LF8RMdI7F2WXe-vTEcwFmicDi9Q6yvRzbYUhL1I19K4PHs9gTHtM-icRbJzPJvm0HKv0gclsXcsQtZgHNFTm3uLYbV5azYu5g5a5Iey-H1dvtpfbIQ.cwy_JXzA6_ZrdDETzYuPbw.o0qbcF7WeGansYSyRR3nZ82MRVoGnyVt4MdZZ00IgGw.vDZidX32xbA7uEUa7GMyiA.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+        assertFalse(Finder.checkJwtPattern(token));
+    }
+
 }
